2885f0264f1e8f419e0f568a48e8a09960233262
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-04 09:59:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,6 +15,6 @@ class App < Sinatra::Base
    get '/say/:number/:phrase' do
      phrase = params[:phrase]
      num= params[:number].to_i
-    "#{phrase}*#{num}"
+    # "#{phrase}*#{num}"
   end
 end
