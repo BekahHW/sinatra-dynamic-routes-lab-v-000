a3f91d0551e1bc5896c1765aa172375bf45e9ae3
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-04 10:04:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,4 +21,13 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
+  
+  get '/:operation/:number1/:number2'
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    
+    num1 params[:operation] num2
+  end
+  
+  
 end
